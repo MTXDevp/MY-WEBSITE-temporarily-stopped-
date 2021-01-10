55b4470ee778c38872176cf960f9495746260c09
MTX <MTXDevp@hotmail.com>
2021-01-10 12:29:40 +0100

    clear

--- README.md
+++
@@ -1,2 +0,0 @@
-# MY-WEBSITE
-Angular + stencil components
--- test-library/projects/ngx-test-library/src/ngx-wp.ts
+++
@@ -1,7 +0,0 @@
-/*
- * Public API Surface of ngx-test-library
- */
-
-export * from './lib/ngx-test-library.service';
-export * from './lib/ngx-test-library.component';
-export * from './lib/ngx-test-library.module';
